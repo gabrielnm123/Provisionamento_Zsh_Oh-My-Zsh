921119d62320e2a0f5c5a32858689874f40edf29
gabrielnm123 <gabrielnmatos96@gmail.com>
2023-06-14 18:57:10 -0300

    removendo o SPACESHIP_PROMPT_ORDER pra usar a ordem padrão de seçoes

--- provision.sh
+++ provision.sh
@@ -18,19 +18,7 @@ ln -s "$HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-
 sed -i '11s/.*/ZSH_THEME="spaceship"/' ~/.zshrc
 
 # Configurando o spaceship, desabilitando algumas funções que requerem mais processamento e que podem fazer o carregamento das pastas demorarem
-echo 'SPACESHIP_PROMPT_ORDER=(
-  user          # Username section
-  dir           # Current directory section
-  host          # Hostname section
-  git           # Git section (git_branch + git_status)
-  hg            # Mercurial section (hg_branch  + hg_status)
-  exec_time     # Execution time
-  line_sep      # Line break
-  jobs          # Background jobs indicator
-  exit_code     # Exit code section
-  char          # Prompt character
-)
-SPACESHIP_USER_SHOW=always
+echo 'SPACESHIP_USER_SHOW=always
 SPACESHIP_PROMPT_ADD_NEWLINE=false
 SPACESHIP_CHAR_SYMBOL="❯"
 SPACESHIP_CHAR_SUFFIX=" "' | cat >> ~/.zshrc
