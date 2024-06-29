e9e6a3cf6ee5c0e78a239e375cffc356e7e12e03
Gabriel Nunes <70154660+gabrielnm123@users.noreply.github.com>
2024-06-29 19:18:31 -0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,11 +4,15 @@
 sudo apt update
 sudo apt install -y zsh curl git
 
-# Instala Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Definir variável ZSH_CUSTOM se não estiver definida
 ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+# Mudar para zsh
+chsh -s $(which zsh)
+exec zsh
+
+# Instala Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Certifique-se de que o diretório $ZSH_CUSTOM existe
 mkdir -p "$ZSH_CUSTOM/themes"
@@ -29,7 +33,7 @@ SPACESHIP_CHAR_SYMBOL="❯"
 SPACESHIP_CHAR_SUFFIX=" "' >> ~/.zshrc
 
 # Baixando e instalando o ZInit
-zsh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # Com o ZInit serão instalados três plugins
 echo 'zinit light zdharma/fast-syntax-highlighting
@@ -39,17 +43,5 @@ zinit light zsh-users/zsh-completions' >> ~/.zshrc
 # Recarrega as configurações do Zsh
 source ~/.zshrc
 
-## Mudando o shell do usuário logado para Zsh
-
-# Obtém o nome de usuário atual
-username=$(whoami)
-
-# Obtém o caminho do shell Zsh
-zsh_path=$(which zsh)
-
-# Altera o shell do usuário no arquivo /etc/passwd
-sudo usermod --shell $zsh_path $username
-
 # Notifica o usuário
-echo "O shell padrão do usuário $username foi alterado para Zsh."
-echo "Por favor, faça logout e login novamente para usar o novo shell."
+echo "O shell padrão do usuário foi alterado para Zsh. Por favor, faça logout e login novamente para usar o novo shell."
