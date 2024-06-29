f0596c57460314fe78d77580af0a753c312be353
Gabriel Nunes <70154660+gabrielnm123@users.noreply.github.com>
2024-06-29 19:12:04 -0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
-# Instala dependencias
-apt update
-apt install -y zsh curl git
+# Instala dependências
+sudo apt update
+sudo apt install -y zsh curl git
 
 # Instala Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Definir variável ZSH_CUSTOM se não estiver definida
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+# Certifique-se de que o diretório $ZSH_CUSTOM existe
+mkdir -p "$ZSH_CUSTOM/themes"
+
 # Clona spaceship-prompt, tema para zsh, para $ZSH_CUSTOM/themes/spaceship-prompt
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 
-# Cria link simbólico do arquivo spaceship.zsh-theme, que esta na pasta clonada, para a pasta themes do $ZSH_CUSTOM
+# Cria link simbólico do arquivo spaceship.zsh-theme, que está na pasta clonada, para a pasta themes do $ZSH_CUSTOM
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-# Troca o tema de devcontainers para spaceship
-sed -i '11s/.*/ZSH_THEME="spaceship"/' ~/.zshrc
+# Troca o tema atual para spaceship
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="spaceship"/' ~/.zshrc
 
 # Configurando o spaceship
 echo 'SPACESHIP_USER_SHOW=always
 SPACESHIP_PROMPT_ADD_NEWLINE=false
 SPACESHIP_CHAR_SYMBOL="❯"
-SPACESHIP_CHAR_SUFFIX=" "' | cat - ~/.zshrc > temp && mv temp ~/.zshrc
+SPACESHIP_CHAR_SUFFIX=" "' >> ~/.zshrc
 
-# Bixando e instalando o ZInit
+# Baixando e instalando o ZInit
 zsh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
-# Com o ZInit será instalado três plugins
-# zdharma/fast-syntax-highlighting: Adiciona syntax highlighting na hora da escrita de comandos que # facilita principalmente em reconhecer comandos que foram digitados de forma incorreta;
-# zsh-users/zsh-autosuggestions: Sugere comandos baseados no histórico de execução conforme você vai # digitando;
-# zsh-users/zsh-completions: Adiciona milhares de completitions para ferramentas comuns como Yarn, # Homebrew, NVM, Node, etc, para você precisar apenas apertar TAB para completar comandos;
+# Com o ZInit serão instalados três plugins
 echo 'zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
-zinit light zsh-users/zsh-completions' | cat >> ~/.zshrc
+zinit light zsh-users/zsh-completions' >> ~/.zshrc
+
+# Recarrega as configurações do Zsh
 source ~/.zshrc
 
 ## Mudando o shell do usuário logado para Zsh
@@ -43,7 +48,7 @@ username=$(whoami)
 zsh_path=$(which zsh)
 
 # Altera o shell do usuário no arquivo /etc/passwd
-usermod --shell $zsh_path $username
+sudo usermod --shell $zsh_path $username
 
 # Notifica o usuário
 echo "O shell padrão do usuário $username foi alterado para Zsh."
